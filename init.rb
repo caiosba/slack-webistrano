da777c5bffbc848cbcb849899e607006c7c584e6
Caio SBA <caiosba@gmail.com>
2014-04-08 10:10:00 +0300

    Notify when a deployment starts

--- init.rb
+++ init.rb
@@ -1,15 +1,33 @@
 require 'webistrano_slack'
 
 Deployment.class_eval do
-  alias complete_with_error_original! complete_with_error!
+
+  alias :complete_with_error_original! :complete_with_error!
   def complete_with_error!
     complete_with_error_original!
-    WebistranoSlack.send_to_slack(self, WebistranoConfig[:slack_settings], false) unless WebistranoConfig[:slack_settings].nil?
+    message = "[#{self.stage.project.name}] #{self.user.login} deployed to #{self.stage.name} with errors! <#{self.url}|View log>"
+    WebistranoSlack.send_to_slack(self, WebistranoConfig[:slack_settings], message) unless WebistranoConfig[:slack_settings].nil?
   end
 
-  alias complete_successfully_original! complete_successfully!
+  alias :complete_successfully_original! :complete_successfully!
   def complete_successfully!
     complete_successfully_original!
-    WebistranoSlack.send_to_slack(self, WebistranoConfig[:slack_settings], true) unless WebistranoConfig[:slack_settings].nil?
+    message = "[#{self.stage.project.name}] #{self.user.login} deployed to #{self.stage.name} successfully! <#{self.url}|View log>"
+    WebistranoSlack.send_to_slack(self, WebistranoConfig[:slack_settings], message) unless WebistranoConfig[:slack_settings].nil?
+  end
+
+  def url
+    # FIXME: Better to try to call project_stage_deployment_path here
+    path = "projects/#{self.stage.project.id}/stages/#{self.stage.id}/deployments/#{self.id}"
+    URI.join WebistranoConfig[:slack_settings][:webistrano_host], path
+  end
+end
+
+Webistrano::Deployer.class_eval do
+  alias execute_original! execute!
+  def execute!
+    message = "[#{deployment.stage.project.name}] #{deployment.user.login} started deploying to #{deployment.stage.name}. <#{deployment.url}|View log>"
+    WebistranoSlack.send_to_slack(deployment, WebistranoConfig[:slack_settings], message) unless WebistranoConfig[:slack_settings].nil?
+    execute_original!
   end
 end
